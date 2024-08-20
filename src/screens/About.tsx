a9605625bbb6d7a13ab6e5b92a119e96a5b85699
Bayu Yuhartono <bayuyuhartono@gmail.com>
2024-08-21 02:02:29 +0700

    feat: pict

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -16,7 +16,7 @@ const About = () => {
           </div>
           <div className="grid grid-cols-3 gap-4 pl-8 pr-8 z-20">
             <div className="">
-              <div className="h-fit mt-8 mb-8">
+              <div className="h-fit">
                 <p className="text-l text-uismoke max-w-96">
                   <p className="text-3xl font-bold">Hello...</p>
                   My name is Bayu, Originating form Indonesia, I'm a Mobile Developer who has experience in developing mobile and website applications, able to work in teams and individually also able to adapt quickly in understanding new things so that they can be relied on in collaboration. Experienced in building the applications with Swift, React Native and React JS
@@ -46,18 +46,12 @@ const About = () => {
                 className="object-cover h-[450px] w-[450px] rounded-full" 
               />
             </div>
-            <div className="">
+            <div className="pl-16">
               <p className="text-xl text-uismoke mb-4">Professional Experience</p>
               <Accordion />
             </div>
           </div>
         </div>
-        {/* <div className="z-10 bottom-0 absolute w-full flex justify-center">
-          <img 
-            src="https://png.pngtree.com/png-vector/20230918/ourmid/pngtree-man-in-shirt-smiles-and-gives-thumbs-up-to-show-approval-png-image_10094392.png" 
-            className="h-[630px]" 
-          />
-        </div> */}
       </div>
     </>
   )
